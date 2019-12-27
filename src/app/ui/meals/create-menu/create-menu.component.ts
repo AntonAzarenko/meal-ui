78550e04d0bf74a530cf00e28ad94ddd8035558b
anton Azarenka <anton_azarenka@epam.com>
2019-12-27 17:51:43 +0300

    ajust menu window

--- src/app/ui/meals/create-menu/create-menu.component.ts
+++ src/app/ui/meals/create-menu/create-menu.component.ts
@@ -5,6 +5,7 @@ import { FoodService } from 'src/app/services/food.service';
 import { Meal } from '../../common/domain/Meal';
 import { MenuService } from 'src/app/services/menu.service';
 import { Food } from '../../common/domain/Food';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-create-menu',
@@ -13,6 +14,7 @@ import { Food } from '../../common/domain/Food';
 })
 export class CreateMenuComponent implements OnInit {
 
+  meal: Meal;
   datasource: Meal[];
   dayOfWeek: string;
   timeOfDay: string;
@@ -21,10 +23,14 @@ export class CreateMenuComponent implements OnInit {
   menus: object[];
   menuTitle: string;
   foodDescription: string;
+  count: number;
+  isNewMenu = true;
 
-  displayedColumns: string[] = ['day', 'meal', 'food', 'count'];
-
-  constructor(private router: Router, private foodService: FoodService, private menuService: MenuService) {
+  constructor(private router: Router,
+     private foodService: FoodService, 
+     private menuService: MenuService,
+     public snackBar: MatSnackBar,
+     private service: MenuService) {
   }
 
   ngOnInit() {
@@ -41,6 +47,9 @@ export class CreateMenuComponent implements OnInit {
   getallMenus(){
     this.menuService.getAllMenus().subscribe((data:any[]) => {
       this.menus = (data);
+      if(this.menus != null){
+          this.isNewMenu = false;
+      }
     });
   }
 
@@ -52,5 +61,58 @@ export class CreateMenuComponent implements OnInit {
     this.menuService.getMenu(this.menuTitle).subscribe((data:Meal[]) => {
       this.datasource = (data);
     });
+  }
+
+  addMeal() {
+    if (this.checkMeal()) {
+      this.appendMeal();
+      this.service.save(this.meal).subscribe((data: any) => {
+        //this.menu.push(data);
+        this.snackBar.open('Еда добавлена в меню', 'Ok', {
+          duration: 2000,
+        });
+      });
+      this.updateMenu(this.menuTitle);
+      this.clearTable();
+    } else {
+      this.snackBar.open('Заполните все поля', 'ERROR', {
+        duration: 2000,
+      });
+    }
+  }
+
+  private checkMeal(): boolean {
+    if (this.dayOfWeek != null && this.menuTitle != null && this.timeOfDay != null && this.food != null) {
+      return true;
+    }
+    return false;
+  }
+
+  private appendMeal(): void {
+    console.log(this.dayOfWeek)
+    console.log(this.menuTitle)
+    console.log(this.timeOfDay)
+    console.log(this.food)
+    this.meal = new Meal();
+    this.meal.count = this.count;
+    this.meal.menuTitle = this.menuTitle;
+    this.meal.meal = this.timeOfDay;
+    this.meal.food = this.food.title;
+    this.meal.day = this.dayOfWeek;
+    console.log(this.meal);
+  }
+
+  clearInput() {
+    this.count = null;
+  }
+
+  addMenuTitle(value: string){
+    this.isNewMenu = true;
+    this.menuTitle = value;
+  }
+
+  private clearTable() {
+    this.meal = null;
+    this.clearInput();
   }
 }
--- src/app/ui/meals/meal-create-info/meal-create-info.component.ts
+++ src/app/ui/meals/meal-create-info/meal-create-info.component.ts
@@ -13,9 +13,6 @@ import {any} from 'codelyzer/util/function';
 })
 export class MealCreateInfoComponent implements OnInit {
 
-  @Input() day: string;
-  @Input() time: string;
-  @Input() meal: Meal;
 
   count: number;
   menuTitle: string;
@@ -26,67 +23,5 @@ export class MealCreateInfoComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.createMenu.updateMenu(any())
-  }
-
-  onKey(event: any) {
-    this.createMenu.getAllFoods();
-    if(!isNaN(event.target.value * 1)) {
-      if (this.meal.carbohydrates != 0) {
-        this.meal.carbohydrates = event.target.value * this.meal.carbohydrates;
-      } else if (this.meal.fats != 0) {
-        this.meal.fats = event.target.value * this.meal.fats;
-      } else if (this.meal.protein != 0) {
-        this.meal.protein = event.target.value * this.meal.protein;
-      }
-    }else {
-      this.snackBar.open('Введите число', 'error', {
-        duration: 2000,
-      });
-    }
-    //this.clearInput();
-  }
-
-  addMeal() {
-    if (this.checkMeal()) {
-      this.appendMeal();
-      console.log(this.meal);
-      this.service.save(this.meal).subscribe((data: any) => {
-        //this.menu.push(data);
-        this.snackBar.open('Еда добавлена в меню', 'Ok', {
-          duration: 2000,
-        });
-      });
-      this.createMenu.updateMenu(this.menuTitle);
-      this.clearTable();
-    } else {
-      this.snackBar.open('Заполните все поля', 'ERROR', {
-        duration: 2000,
-      });
-    }
-  }
-
-  private appendMeal(): void {
-    this.meal.day = this.day;
-    this.meal.count = this.count;
-    this.meal.menuTitle = this.menuTitle;
-    this.meal.meal = this.time;
-    this.meal.food = this.meal.title;
-  }
-
-  clearInput() {
-    this.count = null;
-  }
-
-  private clearTable() {
-    this.meal = null;
-    this.clearInput();
-  }
-
-  private checkMeal(): boolean {
-    if (this.day != null && this.menuTitle != null && this.time != null && this.meal != null) {
-      return true;
-    }
-    return false;
   }
 }
